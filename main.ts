44c01a9d85e5bb5840488b58c27ee2236e5878e0
Babet7777 <94986798+Babet7777@users.noreply.github.com>
2021-11-24 19:32:21 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
+function BotonBPulsado () {
+    basic.pause(50)
+    if (input.buttonIsPressed(Button.AB)) {
+        PoneACero()
+    } else {
+        Contador = Contador - 1
+        led.plot(4, 2)
+        Espera()
+    }
+}
+function PoneACero () {
+    Contador = 0
+    led.plot(0, 2)
+    led.plot(4, 2)
+    Espera()
+}
+function BotonAPulsado () {
+    basic.pause(50)
+    if (input.buttonIsPressed(Button.AB)) {
+        PoneACero()
+    } else {
+        Contador = Contador + 1
+        led.plot(0, 2)
+        Espera()
+    }
+}
+function Espera () {
+    basic.pause(100)
+    led.unplot(0, 2)
+    led.unplot(4, 2)
+    basic.showNumber(Contador)
+}
 let Contador = 0
+Contador = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        BotonAPulsado()
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        BotonBPulsado()
+    }
+})
